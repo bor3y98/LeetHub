a5c9af7c17d4dd10e37079ff2596256bd1ac9c8d
bor3y98 <50171235+bor3y98@users.noreply.github.com>
2022-07-24 13:54:51 +0200

    Time: 5167 ms (14.60%), Space: 15.9 MB (88.82%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.py
+++ 888-fair-candy-swap/888-fair-candy-swap.py
@@ -1,8 +1,7 @@
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
         optimal_sum = int((sum(aliceSizes) + sum(bobSizes)) / 2)
-        print('optimal_sum')
-        print(optimal_sum)
+        
         if sum(aliceSizes) > sum(bobSizes):
             diff_sum = sum(aliceSizes) - optimal_sum
             large_arr = aliceSizes
@@ -13,19 +12,9 @@ class Solution:
             large_arr = bobSizes
             small_arr = aliceSizes
             first = False
-        print('diff_sum')
-        print(diff_sum)
-            
-        # target_element = optimal_sum - diff_sum
-        # print('target_element')
-        print('large_arr')
-        print(large_arr)
-        print(small_arr)
         
         for element in large_arr:
             if element - diff_sum in small_arr:
-                print(element)
-                print(diff_sum)
                 if not first:
                     return [element - diff_sum, element]
                 return [element, element - diff_sum]
