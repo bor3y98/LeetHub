72785565da70b3976680b55e2009a9f7290caf5e
bor3y98 <50171235+bor3y98@users.noreply.github.com>
2022-07-24 11:09:28 +0200

    Time: 25 ms (62.14%), Space: 13.9 MB (8.86%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -16,15 +16,19 @@ class Solution(object):
         current_node=head
         counter=1
         nodes_list=[]
-        while current_node:
-            if counter>=left and counter<=right:
+        while True:
+            if counter>right:
+                break
+            if counter>=left:
                 nodes_list.append(current_node.val)
             current_node=current_node.next
             counter+=1
         current_node=head
         counter=1
-        while current_node:
-            if counter>=left and counter<=right:
+        while True:
+            if counter>right:
+                break
+            if counter>=left :
                 current_node.val=nodes_list.pop()
             current_node=current_node.next
             counter+=1
