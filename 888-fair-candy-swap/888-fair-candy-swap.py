88ddd8596bc944aaf57c8a26b33f9f67db1b990f
bor3y98 <50171235+bor3y98@users.noreply.github.com>
2022-07-28 12:19:34 +0200

    Time: 446 ms (79.74%), Space: 16.7 MB (22.24%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.py
+++ 888-fair-candy-swap/888-fair-candy-swap.py
@@ -1,22 +1,18 @@
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
-        optimal_sum = int((sum(aliceSizes) + sum(bobSizes)) / 2)
-        
-        if sum(aliceSizes) > sum(bobSizes):
-            diff_sum = sum(aliceSizes) - optimal_sum
-            large_arr = aliceSizes
-            small_arr = bobSizes
-            first = True
-        else:
-            diff_sum = sum(bobSizes) - optimal_sum
-            large_arr = bobSizes
-            small_arr = aliceSizes
-            first = False
-        
-        for element in large_arr:
-            if element - diff_sum in small_arr:
-                if not first:
-                    return [element - diff_sum, element]
-                return [element, element - diff_sum]
-            
-        
+        sum1=sum(aliceSizes)
+        total_sum = sum1 + sum(bobSizes)
+        median = total_sum/2
+        dict_alices = {}
+        dict_bob = {}
+        for item in aliceSizes:
+            dict_alices[item-sum1]=item
+        print(dict_alices)
+        for item in bobSizes:
+            dict_bob[item]=int(item-median)
+
+            if dict_bob[item] in dict_alices:
+                print(dict_bob[item])
+                print(dict_alices[dict_bob[item]])
+
+                return[dict_alices[dict_bob[item]],item]
